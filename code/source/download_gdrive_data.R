2a99824d78da0e9be7d888edcef0a1bcd0fe2d29
Samantha Franks Mac <samantha.franks@bto.org>
2023-11-24 16:43:58 +0000

    Created separate code to 1) update; 2) load google drive data
    In case updating not needed every time.

---
+++ code/source/download_gdrive_data.R
@@ -0,0 +1,59 @@
+
+
+# SOURCE CODE to download latest files from Google Drive
+
+# individual metadata
+googledrive::drive_download(
+  file = "https://docs.google.com/spreadsheets/d/10mZu0PAjuIXtCZMSYFUFxa2j-2QxlDDlTQDfNTArf-8/edit?usp=sharing",
+  type = "csv",
+  overwrite = TRUE,
+  path = file.path(datawd, "headstart_curlew_individual_metadata.csv")
+)
+
+# biometric data
+googledrive::drive_download(
+  file = "https://docs.google.com/spreadsheets/d/1FqUZHwe2aLJlw-SiwJtdORtuISnk7tIHlUIT_ZR7pJg/edit?usp=sharing",
+  type = "csv",
+  overwrite = TRUE,
+  path = file.path(datawd, "headstart_curlew_biometric_data.csv")
+)
+
+# gps tag deployment data
+googledrive::drive_download(
+  file = "https://docs.google.com/spreadsheets/d/1V3QMfRHw1Y_yxKeV83CgV-dojuqG0CfIiIy2-D4lK4k/edit?usp=sharing",
+  type = "csv",
+  overwrite = TRUE,
+  path = file.path(datawd, "headstart_curlew_tag_deployment_data_gps.csv")
+)
+
+# radio tag deployment data
+googledrive::drive_download(
+  file = "https://docs.google.com/spreadsheets/d/1_3v4_vnuXvshp3kjDC-U_hzY9-uMuGH4qpJnmmugbNQ/edit?usp=sharing",
+  type = "csv",
+  overwrite = TRUE,
+  path = file.path(datawd, "headstart_curlew_tag_deployment_data_radio.csv")
+)
+
+# resighting data
+googledrive::drive_download(
+  file = "https://docs.google.com/spreadsheets/d/1v-U_m87Nza0P3fcQiEkwOaELwlbwTdaiepFyXUJQhpU/edit?usp=sharing",
+  type = "csv",
+  overwrite = TRUE,
+  path = file.path(datawd, "headstart_curlew_resighting_data.csv")
+)
+
+# egg collection data
+googledrive::drive_download(
+  file = "https://docs.google.com/spreadsheets/d/18JzojuDbjbuqsboZ5clSNgtX5iOXu1ikR_J8FQptzUA/edit?usp=sharing",
+  type = "csv",
+  overwrite = TRUE,
+  path = file.path(datawd, "headstart_curlew_egg_collection_data.csv")
+)
+
+# rearing data
+googledrive::drive_download(
+  file = "https://docs.google.com/spreadsheets/d/1cpXhZwueeV5TqP4wANnWIxW6U8KSdmCvClKf4E-To2Y/edit?usp=sharing",
+  type = "csv",
+  overwrite = TRUE,
+  path = file.path(datawd, "headstart_curlew_rearing_data.csv")
+)
---
+++ code/source/load_gdrive_data.R
@@ -0,0 +1,64 @@
+
+
+# SOURCE CODE to load files from Google Drive
+
+# Load individual metadata
+dt_meta <- read.csv(file.path(datawd, "headstart_curlew_individual_metadata.csv"), header = TRUE, stringsAsFactors = FALSE)
+
+# Load biometric data
+dt_biometric <- read.csv(file.path(datawd, "headstart_curlew_biometric_data.csv"), header = TRUE, stringsAsFactors = FALSE) %>% dplyr::select(1:7, 9:10, 12, 15:16, 18:22, 36:42)
+
+# Load gps tag data
+dt_deploy_gps <- read.csv(file.path(datawd, "headstart_curlew_tag_deployment_data_gps.csv"), header = TRUE, stringsAsFactors = FALSE)
+
+# Load radio tag data
+dt_deploy_radio <- read.csv(file.path(datawd, "headstart_curlew_tag_deployment_data_radio.csv"), header = TRUE, stringsAsFactors = FALSE)
+
+# Load resighting data
+dt_resighting <- read.csv(file.path(datawd, "headstart_curlew_resighting_data.csv"), header = TRUE, stringsAsFactors = FALSE)
+
+# Load egg collection data
+dt_eggs <- read.csv(file.path(datawd, "headstart_curlew_egg_collection_data.csv"), header = TRUE, stringsAsFactors = FALSE)
+
+# Load rearing data
+dt_rearing <- read.csv(file.path(datawd, "headstart_curlew_rearing_data.csv"), header = TRUE, stringsAsFactors = FALSE)
+
+# ----- Create output for DemOn data entry  -----
+
+# Rename biometric fields
+biometric_field_names <- c(
+  "entered_demon",
+  "urn",
+  "day",
+  "month",
+  "year",
+  "site_code",
+  "county_code",
+  "ring",
+  "type",
+  "age",
+  "wing_initials",
+  "wing",
+  "tarsus_toe",
+  "bill",
+  "total_head",
+  "weight",
+  "weight_time",
+  "days_age",
+  "flag_id",
+  "LA",
+  "LB",
+  "RA",
+  "RB",
+  "comments"
+)
+
+names(dt_biometric) <- biometric_field_names
+
+# add date to biometric data
+dt_biometric <- dt_biometric %>% 
+  filter(!is.na(urn)) %>% 
+  mutate(across(.cols = c("day", "month", "year", "LB"), .fns = as.character)) %>% 
+  mutate(date = ifelse(is.na(year), NA, paste(day, month, year, sep="/"))) %>% 
+  mutate(new_date = as.Date(date, tz = "BST", format = c("%d/%m/%Y"))) %>% 
+  mutate(across(.cols = c("day", "month", "year"), .fns = as.integer))
