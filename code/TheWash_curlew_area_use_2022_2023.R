0253382d38a0b216a60b8fbe291adbca83c47246
Hannah Hereward <Hannah.Hereward@bto.org>
2025-02-14 12:09:17 +0000

    New code to extract out the used/avalible plots for The Wash wild curlew tracking data

---
+++ code/TheWash_curlew_area_use_2022_2023.R
@@ -0,0 +1,97 @@
+#### NE103 -- Curlew headstarting project -- using The Wash 2022 and 2023 July-December dataset for wild curlew comparision
+
+## Original code (2021 and 2022) by Gary Clewley
+## Updated for 2023 analysis by Hannah Hereward, Katharine Bowgen, Sam Franks (2023-)
+## NEW SCRIPT copying relevant code from the 2023 analysis - Hannah Hereward, Katharine Bowgen, Sam Franks (2023-)
+
+
+# NOTES ####
+# Analysis of 2022 and 2023 The Wash dataset from 2022 and 2023 July-December using same time periods as the headstarting analysis 
+
+# Check previous reports for example of previous analytical structure to follow
+# Now also includes extracting 'potential breeding season' time periods
+
+# LOAD PACKAGES ####
+load_pkg <- rlang::quos(tidyverse,BTOTrackingTools, here, sp, leaflet, terra)  # quos() function to be lazy on "" around each package
+lapply(lapply(load_pkg, rlang::quo_name), library, character.only = TRUE)
+
+
+
+# SETUP AND CLEAN DATA ####
+
+#read in the wash data here
+#washdat <- 
+
+
+## Make parsable as a Track object by BTOTT package
+names(washdat)[names(washdat)=="individual.local.identifier"]<-"TagID"   # Some tags redeployed multiple times
+names(washdat)[names(washdat)=="timestamp"]<-"DateTime"
+names(washdat)[names(washdat)=="location.long"]<-"longitude"
+names(washdat)[names(washdat)=="location.lat"]<-"latitude"
+names(washdat)[names(washdat)=="gps.satellite.count"]<-"satellites_used"
+
+
+summary(washdat)
+head(washdat)
+
+
+### Coerce required trip and gap columns for later functions (not running trip definition for this project as not central place) ####
+washdat$tripNo<-1; washdat$gap<-0; washdat$gapsec<-1
+
+
+## Check summary of data ####
+summary(washdat)
+
+
+##save data out so I don't keep having to read it all back in and do the tide stuff####
+saveRDS(washdat, here("data/2025 analysis - The Wash/TheWashData.rds"))
+
+
+#Extract meta data???
+
+#Create a flag ID column
+
+##Next in order to be able to left join 'data' and 'dt_meta_gsp' a column needs to be created which matches both together.####
+
+#first extract the tag idea from 'data'
+TagID <- data.frame(TagID = unique(washdat$TagID))
+
+#from this extract out the unique two letter/number code using str_sub
+TagID$flag_id <- str_sub(TagID$TagID, 4,5)
+
+
+
+#clean up a few last things: #####
+###create a dead or stopped transmitting date column to help assess whether some individuals need to be taken out of some categories due to the number of days the remained alive####
+
+
+
+
+# Tidy surplus columns from move:: direct loading #####
+drop_cols<-c("event.id", "visible", "individual.id", "deployment.id", "tag.id", "study.id", "sensor.type.id", "tag.local.identifier", "individual.taxon.canonical.name", "acceleration.raw.x", "acceleration.raw.y", "acceleration.raw.z", "barometric.height", "battery.charge.percent", "battery.charging.current", "gps.hdop", "gps.time.to.fix", "heading", "import.marked.outlier", "light.level", "magnetic.field.raw.x", "magnetic.field.raw.y", "magnetic.field.raw.z", "ornitela.transmission.protocol", "study.name", "sensor.type")
+washdat<- washdat %>% select(-!!drop_cols)
+
+summary(washdat)
+
+##save data out ####
+#saveRDS(washdat, here("data/2025 analysis - The Wash/TheWashData_cleanedup.rds"))
+
+#read back in
+#data <- readRDS(here("data/2025 analysis - The Wash/TheWashData_cleanedup.rds"))
+
+
+####.####
+
+
+#which birds should stay in which category? ####
+#Before we can use the data for analysis it would be useful to add in information to the whole dataset working out which categories the birds best fit into
+#this is because some of the birds died within year of release and subsequent years of release
+
+#NOTE HH additions (14/01/2025) - after discussions with Katharine for year of release birds: we will exclude birds if their fixes do not fully fit within the category 
+# EXCEPTIONS: for 6weeks post release we included anything above 5weeks (35days). For all data from release to end of Dec we included all data if fixes were beyond 31st October
+#this decisions comes to balance wanting to retain as much data as possible but also the coefficent analysis later on comparing individual birds per time period
+
+
+
+
+
